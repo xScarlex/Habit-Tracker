683718872a6afcfbc112ba02639d8c279512841a
xScarlex <cborleo@gmail.com>
2024-03-05 23:48:43 +0000

    05/03/2024

--- HabitTracker/src/HabitTracker/Acciones.java
+++ HabitTracker/src/HabitTracker/Acciones.java
@@ -1,29 +1,58 @@
 package HabitTracker;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Acciones extends JFrame { 
+public class Acciones extends JFrame {
 
-    private JTextField nombreField;
-    private JTextField edadField;
+    private JTextField CamaYVitam;
+    private JTextField Stretch;
+    private JTextField Leer;
+    private JTextField Recoger;
+    private JTextField Teorico;
+    private JTextField Practico;
+    private JTextField Deporte;
+    private JTextField CafeAlc;
+    private JTextField Dia;
+    private JTextField Mes;
+    private JTextField Anio;
     private JButton botonMostrar;
 
     public Acciones() {
-        
+    	
     }
     
     public void menuInicial() {
-    	setTitle("Menú"); 
+        setTitle("Menú");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 200);
+        setSize(300, 400); // Cambié el tamaño para hacerlo más manejable
         setLocationRelativeTo(null);
-        setLayout(new GridLayout(3, 2));
+        setLayout(new GridLayout(12, 2)); // Cambié el GridLayout para adaptarlo a todos los campos
 
-        JLabel nombreLabel = new JLabel("Nombre:");
-        nombreField = new JTextField();
-        JLabel edadLabel = new JLabel("Edad:");
-        edadField = new JTextField();
+        JLabel labelCamaYVitam = new JLabel("Cama y vitaminas: ");
+        CamaYVitam = new JTextField();
+        JLabel labelStretch = new JLabel("Stretch: ");
+        Stretch = new JTextField();
+        JLabel labelLeer = new JLabel("Leer (30 mins): ");
+        Leer = new JTextField();
+        JLabel labelRecoger = new JLabel("Recoger: ");
+        Recoger = new JTextField();
+        JLabel labelEstudioTeorico = new JLabel("Estudio teorico: ");
+        Teorico = new JTextField();
+        JLabel labelEstudioPractico = new JLabel("Estudio practico: ");
+        Practico = new JTextField();
+        JLabel labelDeporte = new JLabel("Deporte: ");
+        Deporte = new JTextField();
+        JLabel labelCafeYAlc = new JLabel("Café y alcohol: ");
+        CafeAlc = new JTextField();
+        JLabel labelDia = new JLabel("Día: ");
+        Dia = new JTextField();
+        JLabel labelMes = new JLabel("Mes: ");
+        Mes = new JTextField();
+        JLabel labelAnio = new JLabel("Año: ");
+        Anio = new JTextField();
+
         botonMostrar = new JButton("Mostrar");
 
         botonMostrar.addActionListener(new ActionListener() {
@@ -32,17 +61,56 @@ public class Acciones extends JFrame {
             }
         });
 
-        add(nombreLabel);
-        add(nombreField);
-        add(edadLabel);
-        add(edadField);
+        add(labelCamaYVitam);
+        add(CamaYVitam);
+        add(labelStretch);
+        add(Stretch);
+        add(labelLeer);
+        add(Leer);
+        add(labelRecoger);
+        add(Recoger);
+        add(labelEstudioTeorico);
+        add(Teorico);
+        add(labelEstudioPractico);
+        add(Practico);
+        add(labelDeporte);
+        add(Deporte);
+        add(labelCafeYAlc);
+        add(CafeAlc);
+        add(labelDia);
+        add(Dia);
+        add(labelMes);
+        add(Mes);
+        add(labelAnio);
+        add(Anio);
         add(botonMostrar);
+
         setVisible(true);
     }
 
     public void mostrarDatos() {
-        String nombre = nombreField.getText();
-        String edad = edadField.getText();
-        JOptionPane.showMessageDialog(this, "Nombre: " + nombre + "\nEdad: " + edad);
+        String camaYVitam = CamaYVitam.getText();
+        String stretch = Stretch.getText();
+        String leer = Leer.getText();
+        String recoger = Recoger.getText();
+        String teorico = Teorico.getText();
+        String practico = Practico.getText();
+        String deporte = Deporte.getText();
+        String cafeAlc = CafeAlc.getText();
+        String dia = Dia.getText();
+        String mes = Mes.getText();
+        String anio = Anio.getText();
+
+        JOptionPane.showMessageDialog(this, "Cama y vitaminas: " + camaYVitam +
+                                            "\nStretch: " + stretch +
+                                            "\nLeer (30 mins): " + leer +
+                                            "\nRecoger: " + recoger +
+                                            "\nEstudio teorico: " + teorico +
+                                            "\nEstudio practico: " + practico +
+                                            "\nDeporte: " + deporte +
+                                            "\nCafé y alcohol: " + cafeAlc +
+                                            "\nDía: " + dia +
+                                            "\nMes: " + mes +
+                                            "\nAño: " + anio);
     }
 }
